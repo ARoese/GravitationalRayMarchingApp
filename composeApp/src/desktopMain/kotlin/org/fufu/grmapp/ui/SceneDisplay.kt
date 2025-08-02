b80f344a5b4d17c5e858e756b5b4eb0c658ab787
Atom Roese <40679658+ARoese@users.noreply.github.com>
2025-08-02 16:35:21 -0400

    allow downloading renders

--- composeApp/src/desktopMain/kotlin/org/fufu/grmapp/ui/SceneDisplay.kt
+++ composeApp/src/desktopMain/kotlin/org/fufu/grmapp/ui/SceneDisplay.kt
@@ -1,5 +1,7 @@
 package org.fufu.grmapp.ui
 
+import androidx.compose.foundation.ContextMenuArea
+import androidx.compose.foundation.ContextMenuItem
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -9,14 +11,22 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.ImageFormat
+import io.github.vinceglb.filekit.dialogs.FileKitDialogSettings
+import io.github.vinceglb.filekit.dialogs.compose.util.encodeToByteArray
+import io.github.vinceglb.filekit.dialogs.openFileSaver
+import io.github.vinceglb.filekit.write
 import io.ktor.network.sockets.InetSocketAddress
 import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.fufu.grmapp.RenderSpec
 import org.fufu.grmapp.renderclient.RenderServer
@@ -54,13 +64,28 @@ fun SceneDisplay(renderSpec: RenderSpec){
     }
     Box(Modifier.fillMaxSize().border(4.dp, Color(255, 0, 0))){
         val currentImage = image
+        val coroutineScope = rememberCoroutineScope()
         if(currentImage != null){
-            Image(
-                currentImage,
-                "example image",
-                Modifier.fillMaxSize(),
-                contentScale = ContentScale.Fit
-            )
+            ContextMenuArea(items = {
+                listOf(
+                    ContextMenuItem("Download"){
+                        coroutineScope.launch {
+                            val saveLocation = FileKit.openFileSaver(
+                                "rendered image",
+                                "png"
+                            )
+                            saveLocation?.write(currentImage.encodeToByteArray(ImageFormat.PNG))
+                        }
+                    }
+                )
+            }){
+                Image(
+                    currentImage,
+                    "example image",
+                    Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+            }
         }
     }
 }
